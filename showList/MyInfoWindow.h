2236ec77be9bb18d29360e14090be4e78b5a5652
Jennifer Louthan <jenniferlouthan@Jennifers-MacBook-Pro.local>
2013-07-22 12:02:27 -0700

    responsive resizing of info windows

--- showList/MyInfoWindow.h
+++ showList/MyInfoWindow.h
@@ -14,10 +14,12 @@
 @interface MyInfoWindow : UIView
 
 @property (nonatomic, strong) UILabel *titleLabel;
+@property (nonatomic, strong) UILabel *venueLabel;
 @property (nonatomic, strong) UILabel *infoLabel;
 //@property (nonatomic, strong) UIButton *detailsButton;
 @property (nonatomic, strong) Event *event;
 
 -(id)initWithEvent:(Event *)event;
+-(void)setFrameWithThreeLabels:(UILabel *)label1 label2:(UILabel *)label2 label3:(UILabel *)label3 textWidth:(CGFloat)width margin:(CGFloat)margin;
 //-(void)buttonPressed;
 @end
--- showList/MyInfoWindow.m
+++ showList/MyInfoWindow.m
@@ -15,45 +15,63 @@
 @implementation MyInfoWindow
 
 - (id)initWithEvent:(Event *)event{
-    self = [super initWithFrame:CGRectMake(0, 0, 200, 145)];
+    self = [super init];
     if (self) {
         // Initialization code
         
         self.event = event;
-        
         [self setBackgroundColor:[UIColor whiteColor]];
-        self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 180, 100)];
+        
+        self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(5, 5, 180, 100)];
         [self.titleLabel setNumberOfLines:0];
         [self.titleLabel setFont:[UIFont boldSystemFontOfSize:12.0]];
         [self.titleLabel setTextAlignment:NSTextAlignmentCenter];
         [self addSubview:self.titleLabel];
-//        
-//        self.detailsButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-//        self.detailsButton.frame = CGRectMake(10, 120, 180, 24);
-//        self.detailsButton.clipsToBounds = YES;
-//
-//        [self.detailsButton setTitle:@"more details" forState:UIControlStateNormal];
-//        [self.detailsButton addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
-//        [self addSubview:self.detailsButton];
         
-        self.infoLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 112, 180, 14)];
+        self.venueLabel = [[UILabel alloc]init];
+        [self.venueLabel setNumberOfLines:0];
+        [self.venueLabel setFont:[UIFont systemFontOfSize:12.0]];
+        [self.venueLabel setTextAlignment:NSTextAlignmentCenter];
+        [self.venueLabel setTextColor:[UIColor greenColor]];
+        [self addSubview:self.venueLabel];
+        
+        self.infoLabel = [[UILabel alloc]init];
         [self.infoLabel setFont:[UIFont systemFontOfSize:12.0]];
         [self.infoLabel setTextAlignment:NSTextAlignmentCenter];
-        [self.infoLabel setTextColor:[UIColor redColor]];
         self.infoLabel.text = @"tap for more details";
         [self addSubview:self.infoLabel];
         
-        self.titleLabel.text = [NSString stringWithFormat: @"%@ \n\n %@",self.event.artistName, self.event.venueName];
+        self.titleLabel.text = [NSString stringWithFormat: @"%@",self.event.artistName];
+        self.venueLabel.text = [NSString stringWithFormat:@"%@", self.event.venueName];
         
-        CGSize labelSize = [self.titleLabel.text sizeWithFont:self.titleLabel.font
-                                                  constrainedToSize:self.titleLabel.frame.size
-                                                      lineBreakMode:self.titleLabel.lineBreakMode];
-        self.titleLabel.frame = CGRectMake(
-                                                 self.titleLabel.frame.origin.x, self.titleLabel.frame.origin.y,
-                                                 self.titleLabel.frame.size.width, labelSize.height);
+        [self setFrameWithThreeLabels:self.titleLabel label2:self.venueLabel label3:self.infoLabel textWidth:190 margin:5];
+
     }
     
     return self;
+}
+
+
+//set size of frames of labels depending on their properties. then sets frame for entire view accordingly
+-(void)setFrameWithThreeLabels:(UILabel *)label1 label2:(UILabel *)label2 label3:(UILabel *)label3 textWidth: (CGFloat)width margin:(CGFloat)margin{
+    CGSize label1Size = [label1.text sizeWithFont:label1.font
+                                              constrainedToSize:label1.frame.size
+                                    lineBreakMode:label1.lineBreakMode];
+    
+    label1.frame = CGRectMake(label1.frame.origin.x, label1.frame.origin.y,
+                                       label1.frame.size.width, label1Size.height);
+    
+    CGSize label2Size = [label2.text sizeWithFont:label2.font constrainedToSize:label2.frame.size lineBreakMode:label2.lineBreakMode];
+    
+    label2.frame = CGRectMake(margin, label1Size.height + margin, width, label2Size.height);
+    
+    CGSize label3Size = [label3.text sizeWithFont:label3.font constrainedToSize:label3.frame.size lineBreakMode:label3.lineBreakMode];
+    
+    label3.frame = CGRectMake(margin, label1Size.height + label2Size.height + margin, width, label3Size.height);
+    
+    CGFloat responsiveHeight = label1Size.height + label2Size.height + 5*margin;
+    
+    self.frame = CGRectMake(0, 0, width+2*margin, responsiveHeight);
 }
 
 //-(void)buttonPressed{
